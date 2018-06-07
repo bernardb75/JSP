c505319f520077376d9aa2be0ae2d4eb931fd0c2
Bernard <bernard.babicz@hotmail.com>
2018-06-07 22:17:00 +0200

    SessionTracking JSP

---
+++ Session Tracking/src/demo/Cart.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Cart {
+private int totalItem=0;
+
+public int getTotalItem() {
+	return totalItem;
+}
+
+public void setTotalItem(int totalItem) {
+	this.totalItem = totalItem;
+}
+
+
+}
